d89bceb66f600ec7aeb8810f17bac264734a3c49
Jason Chen 24079491 <73327854+jchen7381@users.noreply.github.com>
2022-12-12 20:41:04 -0500

    Add files via upload

--- graph.h
+++ graph.h
@@ -52,7 +52,7 @@ public:
     
     void Dijkstra(int V, map<int, vector<pair<int, float>>> adj_list, int S){
         priority_queue<pair<int,float>,vector<pair<int,float>>,greater<pair<int,float>>> pq;
-        vector<int> dist(V);
+        vector<double> dist(V);
         for(unsigned int i = 0;i<V; i++) dist[i] = 1e9;
         
         dist[S] = 0;
@@ -74,6 +74,15 @@ public:
                 }
             }
         
+        }
+        for(unsigned int i = 1; i <= dist.size(); i++){
+            if(dist[i] == 1e9){
+                cout << i << ": not_possible" << endl;
+            }
+            else{
+                cout << i << ": " << S << " cost: " << dist[i] << endl;
+            }
+            
         }
     
     }
